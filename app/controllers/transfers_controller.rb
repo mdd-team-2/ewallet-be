3771412403c01d2b55ea5a22f018f26d31584841
Camilo Dajer <cadajerp@unal.edu.co>
2019-05-04 23:24:23 -0500

    change transaction to mddtransaction

--- app/controllers/transfers_controller.rb
+++ app/controllers/transfers_controller.rb
@@ -1,6 +1,6 @@
 class TransfersController < ApplicationController
   before_action :set_transaction, only: [:show, :update, :destroy]
-  before_action :authenticate_shop_keeper!
+  before_action :authenticate_shop_keeper!, except: [:transfer]
   before_action :authenticate_client!, only: [:transfer]
 
   # GET /transactions
@@ -18,6 +18,67 @@ class TransfersController < ApplicationController
   # POST /transfer
   def transfer
     puts "-----------Transferencia------------"
+    
+    if @current_user.role_id == 1 
+      if Wallet.has_money(@current_user.id, params[:transfer][:value]) 
+        @wallet = Wallet.where(user_id: @current_user.id)
+        if @wallet.id != params[:transfer][:wallet]
+          puts "Tiene el dinero suficiente"
+          @transaction = Mddtransaction.new()
+          puts @current_user.id
+          @transaction.target_id = params[:transfer][:wallet]
+          @transaction.wallet_id = @wallet.id
+          @transaction.transaction_type_id = 1
+          @transaction.amount = params[:transfer][:value]
+
+
+          if @transaction.save
+            ActiveRecord::Base.transaction do
+              Wallet.get_money(@current_user.id, params[:transfer][:value])
+              Wallet.give_money(params[:transfer][:wallet], params[:transfer][:value])
+            end
+            render json: {
+              data: {
+                transaction: @transaction.id,
+                date: @transaction.created_at,
+                amount: @transaction.amount
+              }
+            }, status: :ok
+          else
+            render json: {
+              data: {
+                errors: {
+                  message: "No es posible realizarte transferencias a ti mismo"
+                }
+              }
+            }, status: 204
+          end
+        else
+          render json: @transaction.errors, status: :unprocessable_entity
+        end
+      else
+        render json: {
+          data: {
+            errors: {
+              message: "Saldo insuficiente"
+            }
+          }
+        }, status: 204
+      end
+    else
+      render json: {
+        data: {
+          errors: {
+            message: "No es posible realizar transferencias a tenderos"
+          }
+        }
+      }, status: 204
+    end
+  end
+
+  # POST /transfer
+  def transfer_admin
+    puts "-----------Transferencia desde admin------------"
     
     if @current_user.role_id == 1 
       if Wallet.has_money(@current_user.id, params[:transfer][:value]) 
